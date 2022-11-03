72a97fa1bdd777a8620513cf791b568155c3cd57
Ivette Juarez <ijuarez@deuna.com>
2022-11-03 14:31:59 -0600

    Demo structure is changed, paylod is added to tokenize order

--- checkout-widget-js/checkout-initialize.js
+++ checkout-widget-js/checkout-initialize.js
@@ -1,19 +1,8 @@
-
-const orderTokenReceive = "kpjh0zVDzfwHVGB8GXmsqVN1PyhuXfhjzQTcBF5";
-
 /**
  * Order tokenization
  * @param object order - Order data
  * @return string token
  */
-const getOrderToken = (order) => {
-  //TODO tokenization
-  /*fetch('').then(function (response) {
-        console.log('success!', response);
-    }).catch(function (err) {
-        console.warn('Error', err);
-    });*/
-};
 
 // Initialize checkout
 const dunaCheckout = window.DeunaCheckout();
@@ -22,13 +11,19 @@ const dunaCheckout = window.DeunaCheckout();
  * Open modal checkout widget
  */
 const shouldOpen = async () => {
-  //getOrderToken(order);
-
-  // Configure checkout
-  await dunaCheckout.configure({
-    env: "staging",
-    apiKey: DEUNA_PRIVATE_API_KEY,
-    orderToken: orderTokenReceive,
-  });
-  await dunaCheckout.show();
+  try {
+    const response = await fetch("http://localhost:3000/tokenizeOrder", {
+      method: "POST",
+      mode: "no-cors",
+      body: JSON.stringify(editor.get()),
+    });
+    // Configure checkout
+    await dunaCheckout.configure({
+      env: "staging",
+      apiKey: DEUNA_PRIVATE_API_KEY,
+      orderToken:
+        response.json().token || "kpjh0zVDzfwHVGB8GXmsqVN1PyhuXfhjzQTcBF5",
+    });
+    await dunaCheckout.show();
+  } catch (error) {}
 };
