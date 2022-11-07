b5f8f23b5056f355388400591adbf552e57409ee
Ivette Juarez <ijuarez@deuna.com>
2022-11-07 08:23:04 -0600

    add json editor

--- checkout-widget-react/src/App.js
+++ checkout-widget-react/src/App.js
@@ -1,24 +1,60 @@
+import { useState } from "react";
+
 import "./App.css";
+import JSONInput from "react-json-editor-ajrm";
+import locale from "react-json-editor-ajrm/locale/en";
+import Data from "./mockData.json";
+import ListProducts from "./assets/products.png";
 
 function App() {
+  const [data, setData] = useState(Data);
   /**
    * Open modal checkout widget
    */
   const shouldOpen = async () => {
     // Initialize checkout
     const dunaCheckout = window.DeunaCheckout();
-    // Configure checkout
-    await dunaCheckout.configure({
-      env: "staging",
-      apiKey: process.env.DEUNA_PRIVATE_API_KEY,
-      orderToken: "kpjh0zVDzfwHVGB8GXmsqVN1PyhuXfhjzQTcBF5",
-    });
-    await dunaCheckout.show();
+    try {
+      const response = await fetch("http://localhost:3000/tokenizeOrder", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.token) return;
+
+      // Configure checkout
+      await dunaCheckout.configure({
+        env: "staging",
+        apiKey: process.env.DEUNA_PRIVATE_API_KEY,
+        orderToken: response.json().token,
+      });
+
+      await dunaCheckout.show();
+    } catch (error) {}
   };
 
   return (
     <div className="App">
-      <header className="App-header">
+      <JSONInput
+        id="a_unique_id"
+        placeholder={data}
+        locale={locale}
+        height="100vh"
+        width="70vw"
+      />
+      <div className="shopping-cart">
+        <img
+          className="brand"
+          src="https://uploads-ssl.webflow.com/62e806ed6cc7b20ca6dc2b93/62fca876ea0f2668b1c21b8b_deuna.png"
+          alt=" DEUNA"
+        />
+        <img src={ListProducts} alt="cart" className="cart-list" />
+
+        {/**  Open button checkout widget */}
         <button
           id="button-checkout-deuna"
           className="purchase-btn"
@@ -30,7 +66,7 @@ function App() {
           />
           Checkout
         </button>
-      </header>
+      </div>
     </div>
   );
 }
