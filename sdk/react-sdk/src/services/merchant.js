a2ab36966d325105386cafe9ed9aafd12ff73b30
Carlos yaco <cyaco33@gmail.com>
2022-11-21 10:26:50 -0500

    DX-64:add mock data and set app.jsx

--- sdk/react-sdk/src/services/merchant.js
+++ sdk/react-sdk/src/services/merchant.js
@@ -1,29 +1,38 @@
-import { URL_SERVICES } from "../config";
+//import { URL_SERVICES } from "../config";
 const headers = {
   Accept: "application/json",
   "Content-Type": "application/json",
 };
 const merchantServices = {
   tokenizeOrder: (order, orderId) => {
-    return fetch(`${URL_SERVICES}/tokenizeOrder/${orderId}`, {
-      method: "POST",
-      body: JSON.stringify(order),
-      headers,
-    }).then((response) => response.json());
+    return fetch(
+      `${process.env.REACT_APP_URL_SERVICES}/tokenizeOrder/${orderId}`,
+      {
+        method: "POST",
+        body: JSON.stringify(order),
+        headers,
+      }
+    ).then((response) => response.json());
   },
   setFraudId: (data, orderId) => {
-    return fetch(`${URL_SERVICES}/setFraudId/${orderId}`, {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers,
-    }).then((response) => response.json());
+    return fetch(
+      `${process.env.REACT_APP_URL_SERVICES}/setFraudId/${orderId}`,
+      {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers,
+      }
+    ).then((response) => response.json());
   },
   processPayment: (payload, orderId) => {
-    return fetch(`${URL_SERVICES}/processPayment/${orderId}`, {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers,
-    }).then((response) => response.json());
+    return fetch(
+      `${process.env.REACT_APP_URL_SERVICES}/processPayment/${orderId}`,
+      {
+        method: "POST",
+        body: JSON.stringify(payload),
+        headers,
+      }
+    ).then((response) => response.json());
   },
 };
 
