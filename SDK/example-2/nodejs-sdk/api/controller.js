9aabc491908f3a0afdf975b986b531fcbb89c8b4
Carlos Yaco <cyaco33@gmail.com>
2022-11-30 13:39:22 -0500

    Nodejs and react template is added for sdk2 implementation example, to process an order from backend
    Nodejs and react template is added for sdk2 implementation example, to process an order from backend

---
+++ SDK/example-2/nodejs-sdk/api/controller.js
@@ -0,0 +1,87 @@
+const request = require("request");
+const store = require("../store/db");
+const { Checkout } = require("@deuna/checkout-sdk");
+
+function checkoutSDK() {
+  let checkout;
+
+  // set fraudId and OrderId in store
+  async function setFraudId(fraudId, orderId) {
+    //save data in DB
+    store.push("fraudCredentials", {
+      orderId,
+      fraudId,
+    });
+
+    return {
+      orderId,
+      fraudId,
+    };
+  }
+
+  //get tokenized order
+  async function getTokenizedOrder(orderPayload, orderId) {
+    //get fraudId
+    const orderDB = await store.get("fraudCredentials", orderId);
+    if (!orderDB) {
+      throw new Error("Data not found");
+    }
+    const fraudId = orderDB.fraudId;
+
+    // instance checkout
+    checkout = await Checkout.newInstance({
+      privateApiKey: process.env.DEUNA_PRIVATE_API_KEY,
+      publicApiKey: process.env.DEUNA_PUBLIC_API_KEY,
+      env: process.env.ENVIRONMENT,
+      sessionId: fraudId.sessionId,
+      deviceId: fraudId.deviceId,
+    });
+
+    //get tokenized order
+    const {
+      data: { token, order },
+      error,
+    } = await checkout.requestOrderToken(orderPayload);
+
+    //save data in DB
+    store.push("orders", {
+      token: token,
+      orderId: order.order_id,
+    });
+
+    return {
+      token,
+      order,
+    };
+  }
+
+  //process payment
+  async function processPayment(payload, orderId) {
+    //get Order
+    const orderDB = await store.get("orders", orderId);
+    if (!orderDB) {
+      throw new Error("Data not found");
+    }
+
+    const paymentMethods = await checkout.getPaymentMethods();
+
+    const { data, error } = await checkout.pay(payload.order);
+
+    if (error) {
+      throw new Error(error);
+    }
+    const order = data.order;
+    return {
+      order,
+      paymentMethods,
+    };
+  }
+
+  return {
+    getTokenizedOrder,
+    setFraudId,
+    processPayment,
+  };
+}
+
+module.exports = checkoutSDK();
